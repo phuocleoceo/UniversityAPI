17b7543b6751bc16d24b8885ed422bd502b2ff92
phuocleoceo <ht10082001@gmail.com>
2021-04-22 22:24:07 +0700

    Path Way Repository

---
+++ UniversityAPI/Repository/IRepository/IPathWayRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniversityAPI.Models;
+
+namespace UniversityAPI.Repository.IRepository
+{
+    public interface IPathWayRepository
+    {
+        ICollection<PathWay> GetAll();
+
+        PathWay GetById(int pathWayId);
+
+        bool PathWayExists(string Name);
+
+        bool PathWayExists(int id);
+
+        bool CreatePathWay(PathWay pathWay);
+
+        bool UpdatePathWay(PathWay pathWay);
+
+        bool DeletePathWay(PathWay pathWay);
+
+        bool Save();
+    }
+}
---
+++ UniversityAPI/Repository/PathWayRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniversityAPI.Data;
+using UniversityAPI.Models;
+using UniversityAPI.Repository.IRepository;
+
+namespace UniversityAPI.Repository
+{
+    public class PathWayRepository : IPathWayRepository
+    {
+        private readonly APIContext _db;
+        public PathWayRepository(APIContext db)
+        {
+            _db = db;
+        }
+
+        public bool CreatePathWay(PathWay pathWay)
+        {
+            _db.PathWays.Add(pathWay);
+            return Save();
+        }
+
+        public bool DeletePathWay(PathWay pathWay)
+        {
+            _db.PathWays.Remove(pathWay);
+            return Save();
+        }
+
+        public ICollection<PathWay> GetAll()
+        {
+            return _db.PathWays.ToList();
+        }
+
+        public PathWay GetById(int pathWayId)
+        {
+            return _db.PathWays.FirstOrDefault(c => c.Id == pathWayId);
+        }
+
+        public bool PathWayExists(string Name)
+        {
+            return _db.PathWays.Any(c => c.Name.ToLower().Trim() 
+                                                == Name.ToLower().Trim());            
+        }
+
+        public bool PathWayExists(int id)
+        {
+            return _db.PathWays.Any(c => c.Id == id);
+        }
+
+        public bool UpdatePathWay(PathWay pathWay)
+        {
+            _db.PathWays.Update(pathWay);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+    }
+}
