216a4b0c54577d1cd5fa202f7cbc40eb9f5c2a36
phuocleoceo <ht10082001@gmail.com>
2021-05-01 21:41:54 +0700

    Authentication Setup

--- UniversityAPI/Repository/UserRepository.cs
+++ UniversityAPI/Repository/UserRepository.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using UniversityAPI.Data;
 using UniversityAPI.Models;
@@ -11,9 +16,11 @@ namespace UniversityAPI.Repository
     public class UserRepository : IUserRepository
     {
         private readonly APIContext _db;
-        public UserRepository(APIContext db)
+        private readonly AppSettings _appSettings;
+        public UserRepository(APIContext db, IOptions<AppSettings> appSettings)
         {
             _db = db;
+            _appSettings = appSettings.Value;
         }
 
         public bool IsUniqueUser(string username)
@@ -23,7 +30,28 @@ namespace UniversityAPI.Repository
 
         public User Authenticate(string username, string password)
         {
-            throw new NotImplementedException();
+            var user = _db.Users.SingleOrDefault(c => c.UserName == username && c.Password == password);
+            //User not found
+            if (user == null)
+            {
+                return null;
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name,user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials
+                                        (new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+            return user;
         }
 
         public User Register(string username, string password)
