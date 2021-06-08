b613b85090dff39fb39404a69f4b630150b2d507
phuocleoceo <ht10082001@gmail.com>
2021-06-08 22:54:44 +0700

    Create MVC App and Setup RazorRuntimeComplie

---
+++ UniversityMVC/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversityMVC
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44344/";
+        public static string UniversityAPIPath = APIBaseUrl + "api/university";
+        public static string PathWayAPIPath = APIBaseUrl + "api/pathway";
+    }
+}
