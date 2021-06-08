10df77b2b33abffb189eb3dfd0abb8e92a98b584
phuocleoceo <ht10082001@gmail.com>
2021-06-08 23:50:08 +0700

    Models for MVC

--- UniversityMVC/Models/PathWay.cs
+++ UniversityMVC/Models/PathWay.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace UniversityAPI.Models
+namespace UniversityMVC.Models
 {
     public class PathWay
     {
-        [Key]
         public int Id { get; set; }
 
         [Required]
@@ -19,14 +17,11 @@ namespace UniversityAPI.Models
         public double Distance { get; set; }
 
         public enum DifficultLevel { Easy, Medium, Difficult, Expert }
-
         public DifficultLevel Difficulty { get; set; }
 
         [Required]
         public int UniversityId { get; set; }
-        [ForeignKey(nameof(UniversityId))]
-        public University University { get; set; }
 
-        public DateTime DateCreated { get; set; }
+        public University University { get; set; }
     }
 }
