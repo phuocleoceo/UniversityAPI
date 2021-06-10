67ad74ee2adfceebaf78a9a58a32b8d5746a4640
phuocleoceo <ht10082001@gmail.com>
2021-06-11 00:26:41 +0700

    Login and Register Repository MVC

---
+++ UniversityMVC/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversityMVC.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+
+        public string Token { get; set; }
+    }
+}
---
+++ UniversityMVC/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniversityMVC.Models;
+
+namespace UniversityMVC.Repository.IRepository
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> LoginAsync(string url, User obj);
+
+        Task<bool> RegisterAsync(string url, User obj);
+    }
+}
