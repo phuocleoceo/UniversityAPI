3166331c945a94ccbb83ccfb86113cda9820bb73
phuocleoceo <ht10082001@gmail.com>
2021-06-10 22:38:13 +0700

    Home Page UI

---
+++ UniversityMVC/Models/ViewModel/HomeVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversityMVC.Models.ViewModel
+{
+    public class HomeVM
+    {
+        public IEnumerable<University> UniversityList { get; set; }
+
+        public IEnumerable<PathWay> PathWayList { get; set; }
+    }
+}
