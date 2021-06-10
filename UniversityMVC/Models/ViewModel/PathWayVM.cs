d1463546c849b30763449acd0e2a2f102ad6e274
phuocleoceo <ht10082001@gmail.com>
2021-06-10 19:52:52 +0700

    PathWay ViewModel MVC

---
+++ UniversityMVC/Models/ViewModel/PathWayVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversityMVC.Models.ViewModel
+{
+    public class PathWayVM
+    {
+        public IEnumerable<SelectListItem> UniversityList { get; set; }
+
+        public PathWay PathWay { get; set; }
+    }
+}
